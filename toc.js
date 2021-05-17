346221d4b2c4435b5efb1df843f79aaab0e634db
Alexander <82025750+fiscoal@users.noreply.github.com>
2021-05-17 11:26:03 +0200

    Moved group

--- toc.js
+++ toc.js
@@ -9,25 +9,25 @@ module.exports = {
         'rollout-doc/introduction',
         'rollout-doc/invitation-management/README',
         'rollout-doc/middleware/README',
-        {
-          type: 'category',
-          collapsed: true,
-          label: 'Step-by-step guides',
-          items: [
-            'rollout-doc/middleware/1-a-tse-per-cash-register/README',
-            'rollout-doc/middleware/2-hardware-tses-at-local-server-for-multiple-cash-registers/README',
-            'rollout-doc/middleware/3-hardware-tse-at-the-main-cash-register-for-several-additional-cash-registers/README',
-            'rollout-doc/middleware/4-a-cloud-tse-for-multiple-cash-registers/README',
-            'rollout-doc/middleware/5-rollout-scenario-with-terminals/README',
-            'rollout-doc/middleware/6-data-center-as-operational-environment/README',
-          ]
-        },
         'rollout-doc/shop/README',
         {
         	type: 'category',
             label: 'How To',
             items: [                        
-                'rollout-doc/how-to/how-to-switch-scu'
+                'rollout-doc/how-to/how-to-switch-scu',
+                {
+                  type: 'category',
+                  collapsed: true,
+                  label: 'Step-by-step guides',
+                  items: [
+                    'rollout-doc/middleware/1-a-tse-per-cash-register/README',
+                    'rollout-doc/middleware/2-hardware-tses-at-local-server-for-multiple-cash-registers/README',
+                    'rollout-doc/middleware/3-hardware-tse-at-the-main-cash-register-for-several-additional-cash-registers/README',
+                    'rollout-doc/middleware/4-a-cloud-tse-for-multiple-cash-registers/README',
+                    'rollout-doc/middleware/5-rollout-scenario-with-terminals/README',
+                    'rollout-doc/middleware/6-data-center-as-operational-environment/README',
+                  ]
+                }
             ]
         }
       ]
